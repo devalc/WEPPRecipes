c91b34309bc53c727b739db22d3a9f5ebf038fa4
devalc <deva4998@vandals.uidaho.edu>
2020-04-07 13:40:42 -0700

    added pipe func to namespace

--- R/GOF.R
+++ R/GOF.R
@@ -12,7 +12,7 @@
 #'
 #'
 GOF <- function(df){
-  alltime_GOF_SF <- df %>% summarise(nPairs = nPairs_function(Q_mm_obs, Q_outlet_mm),
+  alltime_GOF_SF <- df %>% dplyr::summarise(nPairs = nPairs_function(Q_mm_obs, Q_outlet_mm),
                                      NSE = NSeff_function(Q_mm_obs, Q_outlet_mm),
                                      DV = DV_function(Q_mm_obs, Q_outlet_mm),
                                      KGE = hydroGOF::KGE(Q_mm_obs, Q_outlet_mm),
@@ -21,7 +21,7 @@ GOF <- function(df){
                                      R2 = R2_function(Q_mm_obs, Q_outlet_mm),
                                      R2adj = R2ad_function(Q_mm_obs, Q_outlet_mm)
   )
-  alltime_GOF_SWE <- df %>% summarise(nPairs = nPairs_function(SWE_mm_obs, SimSWE_mm_wshed),
+  alltime_GOF_SWE <- df %>% dplyr::summarise(nPairs = nPairs_function(SWE_mm_obs, SimSWE_mm_wshed),
                                       NSE = NSeff_function(SWE_mm_obs, SimSWE_mm_wshed),
                                       DV = DV_function(SWE_mm_obs, SimSWE_mm_wshed),
                                       KGE = hydroGOF::KGE(SWE_mm_obs, SimSWE_mm_wshed),
@@ -30,7 +30,7 @@ GOF <- function(df){
                                       R2 = R2_function(SWE_mm_obs, SimSWE_mm_wshed),
                                       R2adj = R2ad_function(SWE_mm_obs, SimSWE_mm_wshed)
   )
-  alltime_GOF_TP_hillslope <- df %>% summarise(nPairs = nPairs_function(TP_kg_day_obs, TP_hill_kg_day),
+  alltime_GOF_TP_hillslope <- df %>% dplyr::summarise(nPairs = nPairs_function(TP_kg_day_obs, TP_hill_kg_day),
                                                NSE = NSeff_function(TP_kg_day_obs, TP_hill_kg_day),
                                                DV = DV_function(TP_kg_day_obs, TP_hill_kg_day),
                                                KGE = hydroGOF::KGE(TP_kg_day_obs, TP_hill_kg_day),
@@ -39,7 +39,7 @@ GOF <- function(df){
                                                R2 = R2_function(TP_kg_day_obs, TP_hill_kg_day),
                                                R2adj = R2ad_function(TP_kg_day_obs, TP_hill_kg_day)
   )
-  alltime_GOF_PP_hillslope <- df %>% summarise(nPairs = nPairs_function(PP_kg_day_obs, PP_hill_kg_day),
+  alltime_GOF_PP_hillslope <- df %>% dplyr::summarise(nPairs = nPairs_function(PP_kg_day_obs, PP_hill_kg_day),
                                                NSE = NSeff_function(PP_kg_day_obs, PP_hill_kg_day),
                                                DV = DV_function(PP_kg_day_obs, PP_hill_kg_day),
                                                KGE = hydroGOF::KGE(PP_kg_day_obs, PP_hill_kg_day),
@@ -48,7 +48,7 @@ GOF <- function(df){
                                                R2 = R2_function(PP_kg_day_obs, PP_hill_kg_day),
                                                R2adj = R2ad_function(PP_kg_day_obs, PP_hill_kg_day)
   )
-  alltime_GOF_SRP_hillslope <- df %>% summarise(nPairs = nPairs_function(SRP_kg_day_obs, SRP_hill_kg_day),
+  alltime_GOF_SRP_hillslope <- df %>% dplyr::summarise(nPairs = nPairs_function(SRP_kg_day_obs, SRP_hill_kg_day),
                                                 NSE = NSeff_function(SRP_kg_day_obs, SRP_hill_kg_day),
                                                 DV = DV_function(SRP_kg_day_obs, SRP_hill_kg_day),
                                                 KGE = hydroGOF::KGE(SRP_kg_day_obs, SRP_hill_kg_day),
@@ -57,7 +57,7 @@ GOF <- function(df){
                                                 R2 = R2_function(SRP_kg_day_obs, SRP_hill_kg_day),
                                                 R2adj = R2ad_function(SRP_kg_day_obs, SRP_hill_kg_day)
   )
-  alltime_GOF_Sediment_hillslope <- df %>% summarise(nPairs = nPairs_function(SSC_tonnes_day_obs, SimSediment_tonnes_day_wshed),
+  alltime_GOF_Sediment_hillslope <- df %>% dplyr::summarise(nPairs = nPairs_function(SSC_tonnes_day_obs, SimSediment_tonnes_day_wshed),
                                                      NSE = NSeff_function(SSC_tonnes_day_obs, SimSediment_tonnes_day_wshed),
                                                      DV = DV_function(SSC_tonnes_day_obs, SimSediment_tonnes_day_wshed),
                                                      KGE = hydroGOF::KGE(SSC_tonnes_day_obs, SimSediment_tonnes_day_wshed),
@@ -66,7 +66,7 @@ GOF <- function(df){
                                                      R2 = R2_function(SSC_tonnes_day_obs, SimSediment_tonnes_day_wshed),
                                                      R2adj = R2ad_function(SSC_tonnes_day_obs, SimSediment_tonnes_day_wshed)
   )
-  alltime_GOF_TP_chan <- df %>% summarise(nPairs = nPairs_function(TP_kg_day_obs, TP_out_kg_day_outlet),
+  alltime_GOF_TP_chan <- df %>% dplyr::summarise(nPairs = nPairs_function(TP_kg_day_obs, TP_out_kg_day_outlet),
                                           NSE = NSeff_function(TP_kg_day_obs, TP_out_kg_day_outlet),
                                           DV = DV_function(TP_kg_day_obs, TP_out_kg_day_outlet),
                                           KGE = hydroGOF::KGE(TP_kg_day_obs, TP_out_kg_day_outlet),
@@ -75,7 +75,7 @@ GOF <- function(df){
                                           R2 = R2_function(TP_kg_day_obs, TP_out_kg_day_outlet),
                                           R2adj = R2ad_function(TP_kg_day_obs, TP_out_kg_day_outlet)
   )
-  alltime_GOF_PP_chan <- df %>% summarise(nPairs = nPairs_function(PP_kg_day_obs, PP_out_kg_day_outlet),
+  alltime_GOF_PP_chan <- df %>% dplyr::summarise(nPairs = nPairs_function(PP_kg_day_obs, PP_out_kg_day_outlet),
                                           NSE = NSeff_function(PP_kg_day_obs, PP_out_kg_day_outlet),
                                           DV = DV_function(PP_kg_day_obs, PP_out_kg_day_outlet),
                                           KGE = hydroGOF::KGE(PP_kg_day_obs, PP_out_kg_day_outlet),
@@ -84,7 +84,7 @@ GOF <- function(df){
                                           R2 = R2_function(PP_kg_day_obs, PP_out_kg_day_outlet),
                                           R2adj = R2ad_function(PP_kg_day_obs, PP_out_kg_day_outlet)
   )
-  alltime_GOF_SRP_chan <- df %>% summarise(nPairs = nPairs_function(SRP_kg_day_obs, SRP_out_kg_day_outlet),
+  alltime_GOF_SRP_chan <- df %>% dplyr::summarise(nPairs = nPairs_function(SRP_kg_day_obs, SRP_out_kg_day_outlet),
                                            NSE = NSeff_function(SRP_kg_day_obs, SRP_out_kg_day_outlet),
                                            DV = DV_function(SRP_kg_day_obs, SRP_out_kg_day_outlet),
                                            KGE = hydroGOF::KGE(SRP_kg_day_obs, SRP_out_kg_day_outlet),
@@ -93,7 +93,7 @@ GOF <- function(df){
                                            R2 = R2_function(SRP_kg_day_obs, SRP_out_kg_day_outlet),
                                            R2adj = R2ad_function(SRP_kg_day_obs, SRP_out_kg_day_outlet)
   )
-  alltime_GOF_Sediment_chan <- df %>% summarise(nPairs = nPairs_function(SSC_tonnes_day_obs, Sediment_tonnes_ebe),
+  alltime_GOF_Sediment_chan <- df %>% dplyr::summarise(nPairs = nPairs_function(SSC_tonnes_day_obs, Sediment_tonnes_ebe),
                                                 NSE = NSeff_function(SSC_tonnes_day_obs, Sediment_tonnes_ebe),
                                                 DV = DV_function(SSC_tonnes_day_obs, Sediment_tonnes_ebe),
                                                 KGE = hydroGOF::KGE(SSC_tonnes_day_obs, Sediment_tonnes_ebe),
