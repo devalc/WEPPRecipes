daa91ec527c7ee424cd3acc9171c78bcda8318f6
devalc <deva4998@vandals.uidaho.edu>
2020-04-08 13:23:34 -0700

    dependencies fix

--- R/est_P.R
+++ R/est_P.R
@@ -17,15 +17,15 @@
 #'
 est_P <- function(totalwatsed_df, par){
   ###par order P_conc_Runoff_guess_mg_l, P_conc_Lateral_guess_mg_l, P_conc_Baseflow_guess_mg_l, P_conc_Sediment_guess_mg_l
-  P_calc <- totalwatsed_df %>% select(Date, WY, Area_m2_wshed, Sed_Del_kg_wshed,
+  P_calc <- totalwatsed_df %>% dplyr::select(Date, WY, Area_m2_wshed, Sed_Del_kg_wshed,
                                       Runoff_m3_wshed_to_mm, Lateral_m3_wshed_to_mm, Baseflow_mm_wshed ) %>%
-    mutate(P_load_mg = Sed_Del_kg_wshed * par[4],
+    dplyr::mutate(P_load_mg = Sed_Del_kg_wshed * par[4],
            P_runoff_mg = (Runoff_m3_wshed_to_mm * Area_m2_wshed * par[1]) ,
            P_lateral_mg = (Lateral_m3_wshed_to_mm * Area_m2_wshed * par[2]) ,
            P_baseflow_mg = (Baseflow_mm_wshed * Area_m2_wshed * par[3]),
            TP_hill_kg_day = (P_load_mg + P_runoff_mg + P_lateral_mg +P_baseflow_mg)/1000000,
            PP_hill_kg_day = P_load_mg/1000000,
-           SRP_hill_kg_day = TP_hill_kg_day - PP_hill_kg_day) %>% select(-Area_m2_wshed, -Sed_Del_kg_wshed, -Runoff_m3_wshed_to_mm,
+           SRP_hill_kg_day = TP_hill_kg_day - PP_hill_kg_day) %>% dplyr::select(-Area_m2_wshed, -Sed_Del_kg_wshed, -Runoff_m3_wshed_to_mm,
                                                                          -Lateral_m3_wshed_to_mm, -Baseflow_mm_wshed)
 
   return(P_calc)
