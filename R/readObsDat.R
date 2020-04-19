4d0c775528931a6990a66d3037dffb72171fccd7
devalc <deva4998@vandals.uidaho.edu>
2020-04-19 13:41:32 -0700

    updated obs data read to adjust to files created by MD

--- R/readObsDat.R
+++ R/readObsDat.R
@@ -3,11 +3,11 @@
 #'  Takes in a observed data in this specific ordered columns and format:
 #'  column1. Date   --format = "%m/%d/%Y"
 #'  column2. Streamflow --Units mm
-#'  column3. Suspended sediment concentration --Units kg/day
+#'  column3. Suspended sediment concentration --Units tonnes/day
 #'  column4. Total Phosphorus --Units kg/day
 #'  column5. Soluble Reactive Phosphorus --Units kg/day
 #'  column6. Particulate Phosphorus --Units kg/day
-#'  column7. Suspended sediment concentration --Units tonnes/day
+#'  column7. WY
 #'  column8. Snow Water Equivalent --Units mm
 #'
 #'
@@ -19,11 +19,22 @@
 
 readObsDat <- function(fpath){
   ObsData <- read.csv(fpath,stringsAsFactors = F)
-  colnames(ObsData) <- c("Date", "Q_mm_obs", "SSC_kg_day_obs",
+  colnames(ObsData) <- c("Date", "Q_mm_obs", "SSC_tonnes_day_obs",
                          "TP_kg_day_obs",  "SRP_kg_day_obs", "PP_kg_day_obs",
-                         "SSC_tonnes_day_obs","SWE_mm_obs")
+                         "WY")
   ObsData$Date <- as.Date(ObsData$Date, format = "%m/%d/%Y")
-  ObsData <- ObsData %>% dplyr::mutate(WY = EflowStats::get_waterYear(ObsData$Date)) %>%
-    dplyr::select(Date, WY, everything())
+  ObsData <- ObsData %>% dplyr::select(Date, WY, everything())
   return(ObsData)
 }
+
+
+# readObsDat <- function(fpath){
+#   ObsData <- read.csv(fpath,stringsAsFactors = F)
+#   colnames(ObsData) <- c("Date", "Q_mm_obs", "SSC_kg_day_obs",
+#                          "TP_kg_day_obs",  "SRP_kg_day_obs", "PP_kg_day_obs",
+#                          "SSC_tonnes_day_obs","SWE_mm_obs")
+#   ObsData$Date <- as.Date(ObsData$Date, format = "%m/%d/%Y")
+#   ObsData <- ObsData %>% dplyr::mutate(WY = EflowStats::get_waterYear(ObsData$Date)) %>%
+#     dplyr::select(Date, WY, everything())
+#   return(ObsData)
+# }
