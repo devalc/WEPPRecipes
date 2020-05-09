bbdd81de409f96f873c7488b5b9285812b750cfa
devalc <deva4998@vandals.uidaho.edu>
2020-05-09 15:22:07 -0700

    added pbias to ts plt

--- R/plot_obs_sim_ts.R
+++ R/plot_obs_sim_ts.R
@@ -13,7 +13,9 @@ plot_obs_sim_ts <- function(x, y1,y2,
 
   nse_val = signif(NSeff_function(y2,y1),2)
   kge_val = signif( hydroGOF::KGE(y2,y1),2)
+  pbias = signif(hydroGOF::pbias(y2,y1),2)
   R2_val = signif(R2_function(y2,y1),2)
+
 
   maxSF1  = max(y1, na.rm = T)
   maxSF2 = max(y2, na.rm = T)
@@ -28,6 +30,7 @@ plot_obs_sim_ts <- function(x, y1,y2,
        ylab = paste(plot.var.name,"(", units, ")", sep = " "),
        main = paste0("NSE:", nse_val, "       ",
                      "KGE:", kge_val, "       ",
+                     "pbias: ", pbias, "       ",
                      "RSq:", R2_val,  "       "), cex.main = 0.9)
   lines(x, y2, col = "black", lwd=2 )
   legend("topright",
